ff541887350b9fc7928e8f058380d53e8b1a751d
N-syaro <koya.syaro@gmail.com>
2026-03-09 15:48:55 +0900

    2d基礎移動

---
+++ Assets/Scripts/Player_Controller.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player_Controller : MonoBehaviour
+{
+    [Header("移動スピード"), SerializeField]
+    private float P_sp = 5f;
+
+    private Vector2 moveInput;
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+    private void Move()
+    {
+        transform.Translate(moveInput * P_sp * Time.deltaTime);
+    }
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+    }
+
+}
