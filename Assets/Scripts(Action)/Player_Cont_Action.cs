4052538c676d3ed478957a6fc46da780afd6e75b
N-syaro <koya.syaro@gmail.com>
2026-03-10 15:04:11 +0900

    2d移動完成

--- Assets/Scripts(Action)/Player_Cont_Action.cs
+++ Assets/Scripts(Action)/Player_Cont_Action.cs
@@ -6,7 +6,16 @@ public class Player_Cont_Action : MonoBehaviour
 {
     [Header("プレイヤースピード"), SerializeField]
     private float P_sp = 5f;
+    [Header("ジャンプパワー"), SerializeField]
+    private float P_jp = 4;
+    [SerializeField]
+    private Transform Gro_Check;
+    [SerializeField]
+    private float Gro_CheckRad = 0.2f;
+    [SerializeField]
+    private LayerMask gro_layer;
 
+    private bool isGrounded;
     private Vector2 Inp_Dire;
     private Rigidbody2D Rb2d;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -18,14 +27,29 @@ public class Player_Cont_Action : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        isGrounded = Physics2D.OverlapCircle(Gro_Check.position, Gro_CheckRad, gro_layer);
         Move();  
     }
     private void Move()
     {
         Rb2d.linearVelocity = new Vector2(Inp_Dire.x * P_sp, Rb2d.linearVelocity.y);
     }
+    private void Jump()
+    {
+        if (isGrounded)
+        {
+            this.Rb2d.AddForce(transform.up * P_jp, ForceMode2D.Impulse);
+        }
+    }
     public void OnMove(InputAction.CallbackContext context)
     {
         Inp_Dire=context.ReadValue<Vector2>();
+    }
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            Jump();
+        }
     }
 }
